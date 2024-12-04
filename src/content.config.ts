85967479c61df5296d09404e083b4c22d34e1250
gesho <geshov@gmail.com>
2024-12-04 20:16:37 +0300

    move blog posts in folders

---
+++ src/content.config.ts
@@ -0,0 +1,9 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./blog" }),
+  // schema:
+});
+
+export const collections = { blog };
