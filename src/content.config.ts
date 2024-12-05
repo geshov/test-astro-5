b05a769fc269a39cd6205bd01ec2429c7dead659
gesho <geshov@gmail.com>
2024-12-05 11:18:04 +0300

    delete unused z import

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,8 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./blog" }),
-  // schema:
 });
 
 export const collections = { blog };
