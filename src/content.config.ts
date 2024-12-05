e95cce8bd92e9869b9bed0ba300f2c81d3e10804
gesho <geshov@gmail.com>
2024-12-05 16:55:16 +0300

    move blog to contetnt folder

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./blog" }),
+  loader: glob({ pattern: "**/*.md", base: "./content/blog" }),
 });
 
 export const collections = { blog };
