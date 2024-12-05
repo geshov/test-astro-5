f41abe87b12c8bff3fe56a8227e5b8597691931c
gesho <geshov@gmail.com>
2024-12-05 14:19:19 +0300

    description vs title

--- src/components/Blog.svelte
+++ src/components/Blog.svelte
@@ -6,11 +6,13 @@
   <div class="prose mx-auto">
     <h1>Блог</h1>
 
-    <div class="space-y-2">
+    <div class="space-y-3">
       {#each blog as post}
         <a href="/posts/{post.id}" class="flex gap-6 link link-primary">
-          <div class="w-24">{post.data.date.toLocaleDateString("ru-RU")}</div>
-          <div>{post.data.title}</div>
+          <div class="w-20 shrink-0">
+            {post.data.date.toLocaleDateString("ru-RU")}
+          </div>
+          <div>{post.data.description}</div>
         </a>
       {/each}
     </div>
