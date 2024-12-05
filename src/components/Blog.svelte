3f23420c8366359ea0936e8ec16d5bb4f017bb19
gesho <geshov@gmail.com>
2024-12-05 09:50:23 +0300

    create svelte blog component

---
+++ src/components/Blog.svelte
@@ -0,0 +1,18 @@
+<script>
+  let { blog } = $props();
+</script>
+
+<div class="p-4">
+  <div class="prose mx-auto">
+    <h1>Блог</h1>
+
+    <div class="space-y-2">
+      {#each blog as post}
+        <a href="/posts/{post.id}" class="flex gap-6 link link-primary">
+          <div class="w-24">{post.data.date.toLocaleDateString()}</div>
+          <div>{post.data.title}</div>
+        </a>
+      {/each}
+    </div>
+  </div>
+</div>
