f61fb67dca07002c5433df30e021c6c34cb7a2ff
gesho <geshov@gmail.com>
2024-12-05 14:04:59 +0300

    fix date format

--- src/components/Blog.svelte
+++ src/components/Blog.svelte
@@ -9,7 +9,7 @@
     <div class="space-y-2">
       {#each blog as post}
         <a href="/posts/{post.id}" class="flex gap-6 link link-primary">
-          <div class="w-24">{post.data.date.toLocaleDateString()}</div>
+          <div class="w-24">{post.data.date.toLocaleDateString("ru-RU")}</div>
           <div>{post.data.title}</div>
         </a>
       {/each}
