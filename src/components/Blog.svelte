8f57e9154ef3e6bf8c8a91dc67ad6f2c52d95ccb
gesho <geshov@gmail.com>
2024-12-06 10:33:34 +0300

    fix blog style

--- src/components/Blog.svelte
+++ src/components/Blog.svelte
@@ -8,24 +8,26 @@
   <div class="prose mx-auto">
     <h1>Блог</h1>
 
-    <div class="space-y-8">
+    <div class="space-y-10">
       {#each blog as post}
-        <a href="/posts/{post.id}" class="flex gap-4 no-underline">
-          <div class="w-1/3 shrink-0">
+        <a
+          href="/posts/{post.id}"
+          class="grid grid-cols-1 sm:grid-cols-3 gap-4 no-underline">
+          <div>
             <Tthumbnail
               id={post.id}
               sizes="(min-width: 635px) 212px, 30vw"
               alt={post.data.title} />
           </div>
 
-          <div class="space-y-2">
-            <div class="text-xs text-info">
+          <div class="space-y-0 sm:col-span-2">
+            <div class="text-sm text-info">
               {post.data.date.toLocaleDateString("ru-RU")}
             </div>
 
-            <div class="link link-primary">
+            <h2 class="link link-primary">
               {post.data.description}
-            </div>
+            </h2>
           </div>
         </a>
       {/each}
