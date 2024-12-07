da8abe24f6d1f4e7412dc419fc7bba8f00848c60
gesho <geshov@gmail.com>
2024-12-07 18:31:43 +0300

    add thumbnail to post page

--- src/components/Blog.svelte
+++ src/components/Blog.svelte
@@ -12,16 +12,16 @@
       {#each blog as post}
         <a
           href="/posts/{post.id}"
-          class="grid grid-cols-1 sm:grid-cols-3 gap-4 no-underline">
-          <div>
+          class="grid grid-cols-1 sm:grid-cols-5 gap-4 no-underline">
+          <div class="sm:col-span-2">
             <Thumbnail
               id={post.id}
-              sizes="(min-width: 640px) 201px, 90vw"
+              sizes="(min-width: 640px) 245px, 90vw"
               alt={post.data.title} />
           </div>
 
-          <div class="space-y-0 sm:col-span-2">
-            <div class="text-sm text-info">
+          <div class="space-y-0 sm:col-span-3">
+            <div class="text-sm">
               {post.data.date.toLocaleDateString("ru-RU")}
             </div>
 
--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -1,10 +1,27 @@
 <script>
+  import Thumbnail from "./Thumbnail.svelte";
+
   let { post, children } = $props();
 </script>
 
 <div class="p-6">
   <div class="prose mx-auto">
     <h1>{post.data.description}</h1>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <div>
+        <Thumbnail
+          id={post.id}
+          sizes="(min-width: 640px) 310px, 90vw"
+          alt={post.data.title} />
+      </div>
+
+      <div class="flex justify-center items-center">
+        <div class="text-2xl font-bold">
+          {post.data.date.toLocaleDateString("ru-RU")}
+        </div>
+      </div>
+    </div>
 
     {@render children()}
   </div>
