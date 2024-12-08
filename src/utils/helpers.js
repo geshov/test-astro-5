12e5d83e371cd611c21e86673ecf2445bb288d7b
gesho <geshov@gmail.com>
2024-12-08 17:58:01 +0300

    move get thumbnail to helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,5 @@
+import { getImage } from "astro:assets";
+
 export const sortPosts = (posts) => {
   posts.sort((a, b) => {
     if (a.data.date < b.data.date) return 1;
@@ -6,4 +8,16 @@ export const sortPosts = (posts) => {
   });
 
   return posts;
+};
+
+export const getThumbnail = async (post, loading = "lazy") => {
+  const { default: src } = await import(
+    `../../content/blog/${post.id}/thumbnail.jpg`
+  );
+
+  return await getImage({
+    src: src,
+    widths: [1000, 800, 600, 400],
+    loading: loading,
+  });
 };
